f26d2e435625ef7844d9117a29df3568e857b4da
platon.AI <galaxyeye@live.cn>
2026-04-04 17:41:27 +0800

    Disable XPath tests due to postponed support

--- pulsar-tests/pulsar-it-tests/src/test/kotlin/ai/platon/pulsar/browser/PulsarWebDriverXPathTests.kt
+++ pulsar-tests/pulsar-it-tests/src/test/kotlin/ai/platon/pulsar/browser/PulsarWebDriverXPathTests.kt
@@ -8,6 +8,7 @@ import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
+@Disabled("XPath support has been postponed")
 class PulsarWebDriverXPathTests : WebDriverTestBase() {
 
     override val webDriverService get() = FastWebDriverService(browserFactory)
